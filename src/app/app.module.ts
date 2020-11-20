acba1d6cf649c5196bfbba94bd551b78dbf95e07
devalavanya <Lavanya.Devabattula@gmail.com>
2020-11-20 19:51:41 +0530

    assessemnt2 wipro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [FormsModule, ReactiveFormsModule,
     BrowserModule
   ],
   providers: [],
